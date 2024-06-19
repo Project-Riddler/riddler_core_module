0691abfc60985dc437c69ebef4ce82e63a1944ea
Don'Von Wiley <donvonwiley@MacBook-Pro-2.local>
2024-06-19 15:07:39 -0400

    initial

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/Project-Riddler/riddler_core_module
+
+go 1.22.1
---
+++ pkg/types/taskPayload.go
@@ -0,0 +1,26 @@
+package types
+
+type PostNotificationTaskPayload struct {
+	Question           string
+	PostID             string
+	UserID             string
+	ConnectionUserID   string
+	IsInternalQuestion bool
+	TopicRounds        [][]string
+}
+
+type ChatNotificationTaskPayload struct {
+	PostID     string
+	FromUserID string
+	ToUserID   string
+	Message    string
+	MessageID  string
+}
+
+// A list of task types.
+const (
+	TypePostNotification   = "post:deliver"
+	TypeChatNotification   = "chat:deliver"
+	TypeSystemNotification = "system:deliver"
+)
+
