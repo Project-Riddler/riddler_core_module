19156508a8e2ddf7b22abb324dd7ce46ab26ece8
Justin Finney <justinfinney@MacBook-Pro-3.local>
2025-03-06 14:25:43 -0500

    Update Core module to support approved friend request notifcation

--- pkg/types/taskPayload.go
+++ pkg/types/taskPayload.go
@@ -26,8 +26,9 @@ type FriendNotificationTaskPayload struct {
 
 // A list of task types.
 const (
-	TypePostNotification   = "post:deliver"
-	TypeChatNotification   = "chat:deliver"
-	TypeFriendNotification = "friend:deliver"
-	TypeSystemNotification = "system:deliver"
+	TypePostNotification      = "post:deliver"
+	TypeChatNotification      = "chat:deliver"
+	TypeFriendNotification    = "friend:deliver"
+	TypeApprovedFriendRequest = "approvedFriendRequest:deliver"
+	TypeSystemNotification    = "system:deliver"
 )
